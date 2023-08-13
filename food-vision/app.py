1b2b9e7283c8ef6680153b6faab29c259f23ec77
dhmo1900 <59896032+dhmo1900@users.noreply.github.com>
2023-08-13 01:42:13 +0100

    ?

--- food-vision/app.py
+++ food-vision/app.py
@@ -7,6 +7,27 @@ import streamlit as st
 import tensorflow as tf
 from utils import load_and_prep_image, classes_and_models, update_logger, predict_json
 
+# try:
+#     import streamlit.ReportThread as ReportThread
+#     from streamlit.server.Server import Server
+# except Exception:
+#     # Streamlit >= 0.65.0
+#     import streamlit.report_thread as ReportThread
+#     from streamlit.server.server import Server
+    
+# s = session_info.session
+# if (
+#     # Streamlit < 0.54.0
+#     (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
+#     or
+#     # Streamlit >= 0.54.0
+#     (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
+#     or
+#     # Streamlit >= 0.65.2
+#     (not hasattr(s, '_main_dg') and s._uploaded_file_mgr == ctx.uploaded_file_mgr)
+# ):
+#     this_session = s
+
 # Setup environment credentials (you'll need to change these)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "daniels-dl-playground-4edbcb2e6e37.json" # change for your GCP key
 PROJECT = "My Project 10000" # change for your GCP project
